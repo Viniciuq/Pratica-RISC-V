1dd3cffc134b4991b6304e5e80de97bfc168ffd1
Viniciuq <111989293+Viniciuq@users.noreply.github.com>
2025-06-09 21:44:37 -0300

    Update datapath.v

--- RISCV_VINI/Verilog/datapath/datapath.v
+++ RISCV_VINI/Verilog/datapath/datapath.v
@@ -3,23 +3,23 @@
 module datapath(clk, reset);
   
   input wire clk, reset;
-  wire [`DATA_WIDTH-1:0] pc_atual, pc_proximo;					        // PC atual e próximo
-  wire [`DATA_WIDTH-1:0] instrucao;                             // Instrução
-  wire ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch;   // Fios para os sinais
+  wire [`DATA_WIDTH-1:0] pc_atual, pc_proximo; // PC atual e próximo
+  wire [`DATA_WIDTH-1:0] instrucao; // Instrução
+  wire ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch; // Fios para os sinais
   wire [1:0] ALUOp;
-  wire [4:0] rs1 = instrucao[19:15];						                // Fios para registradores
+  wire [4:0] rs1 = instrucao[19:15]; // Fios para registradores
   wire [4:0] rs2 = instrucao[24:20];
   wire [4:0] rd  = instrucao[11:7];
   wire [`DATA_WIDTH-1:0] rd1, rd2;
-  wire [`DATA_WIDTH-1:0] imm;									                  // Imediato
-  wire [`DATA_WIDTH-1:0] alu_mux_out;							              // Saida do MUX ALUSrc
-  wire [`DATA_WIDTH-1:0] alu_result;						               	// ALU
+  wire [`DATA_WIDTH-1:0] imm;	// Imediato
+  wire [`DATA_WIDTH-1:0] alu_mux_out;	// Saida do MUX ALUSrc
+  wire [`DATA_WIDTH-1:0] alu_result; // ALU
   wire zero;
-  wire [3:0] alu_opcode;										                    // ALU Control
-  wire [`DATA_WIDTH-1:0] mem_data;							              	// Memória de dados
-  wire [`DATA_WIDTH-1:0] write_data;							              // Dado a ser escrito no reg
-  wire [`DATA_WIDTH-1:0] pc_soma4, pc_alvo;					          	// Somadores de PC
-  wire [`DATA_WIDTH-1:0] pc_mux_out;						              	// Saída do mux PCSrc
+  wire [3:0] alu_opcode; // ALU Control
+  wire [`DATA_WIDTH-1:0] mem_data; // Memória de dados
+  wire [`DATA_WIDTH-1:0] write_data; // Dado a ser escrito no reg
+  wire [`DATA_WIDTH-1:0] pc_soma4, pc_alvo;	// Somadores de PC
+  wire [`DATA_WIDTH-1:0] pc_mux_out; // Saída do mux PCSrc
   
   pc pc_inst(pc_proximo, clk, reset, pc_atual);
   instruction_memory im_inst(pc_atual[9:2], instrucao);
